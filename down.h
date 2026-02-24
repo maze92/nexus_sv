28fb5bed51f26e7790574633c87f12117e18788e
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-02-24 06:44:30 +0000

    Update down.h

--- down.h
+++ down.h
@@ -5,3 +5,4 @@ https://forum.turkmmo.com/konu/3926123-metin2de-canavar-ve-npc-isim-yuksekligi-h
 https://metin2.dev/topic/34299-c-gf-__loadatlasmarkinfo-whitemark-pixel-shift-fix/
 https://metin2.dev/topic/33967-fix-questbuttons-out-of-questwindow-when-multiple-images-are-present/
 https://metin2.dev/topic/16814-fix-for-the-refresh-of-the-skill-cooldown/
+https://metin2.dev/topic/32961-fix-hitting-from-the-top-of-the-horse/
