8f773e8a635a890a1fabe8c8e3bb8db7cd9f8947
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-02-24 03:03:11 +0000

    Add files via upload

---
+++ FoxFS/ArchiveWriter.h
@@ -0,0 +1,49 @@
+#ifndef FOXFS_ARCHIVEWRITER_H
+#define FOXFS_ARCHIVEWRITER_H
+
+#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
+#include <windows.h>
+#ifdef _DEBUG
+	#pragma comment( lib, "cryptlib-8.9.0MTd.lib" )
+	#pragma comment( lib, "Lz4Lib_D.lib" )
+	#pragma comment( lib, "XxhashLib_D.lib" )
+#else
+	#pragma comment( lib, "cryptlib-8.9.0MT.lib" )
+	#pragma comment( lib, "Lz4Lib.lib" )
+	#pragma comment( lib, "XxhashLib.lib" )
+#endif
+#else
+#include <limits.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <fcntl.h>
+#endif
+
+class ArchiveWriter
+{
+public:
+	ArchiveWriter();
+	~ArchiveWriter();
+
+	bool create(const char* filename, const char* keyfile = 0);
+	void close();
+
+	bool add(const char* filename, unsigned int decompressed, unsigned int compressed, unsigned int hash, const void* data);
+
+private:
+	unsigned char key[32];
+	unsigned char iv[32];
+
+#if defined(_WIN32) || defined(_WIN64) || defined(WIN32) || defined(WIN64)
+	CRITICAL_SECTION mutex;
+	HANDLE file;
+	HANDLE keys;
+#else
+	pthread_mutex_t mutex;
+	int file;
+	int keys;
+#endif
+};
+
+#endif
