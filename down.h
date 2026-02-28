f040d4cecb249817a39ea2d1870111b632719622
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-02-28 10:40:41 +0000

    Update down.h

--- down.h
+++ down.h
@@ -1,6 +1,5 @@
 https://forum.turkmmo.com/konu/3938195-ikarus-v2-minimap-mark/
 https://forum.turkmmo.com/konu/3905638-mini-map-yakinlastirma-duzenlemesi/
-https://forum.turkmmo.com/konu/3928135-optmizasyon-16bit-renk-destegini-kaldirmak/
 https://forum.turkmmo.com/konu/3926123-metin2de-canavar-ve-npc-isim-yuksekligi-hatasi-fix/
 https://metin2.dev/topic/34299-c-gf-__loadatlasmarkinfo-whitemark-pixel-shift-fix/
 https://metin2.dev/topic/33967-fix-questbuttons-out-of-questwindow-when-multiple-images-are-present/
