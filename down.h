d5d0df70b8a95994ec507015c0aca26de813ddf8
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-03-01 13:17:57 +0000

    Update down.h

--- down.h
+++ down.h
@@ -14,7 +14,6 @@ https://forum.turkmmo.com/konu/3930936-c-fix-mini-harita-gorev-iconu/
 https://forum.turkmmo.com/konu/3797038-10lu-statu-verme-sistemi/
 https://forum.turkmmo.com/konu/3893813-sonitex-offline-shop/
 https://forum.turkmmo.com/konu/3923817-kostum-takiliyken-cinsiyet-degistiremezsin/
-https://forum.turkmmo.com/konu/3877812-imagebox-memory-leak-fix/
 https://forum.turkmmo.com/konu/3917617-level-yani-gm-ikonu/
 https://forum.turkmmo.com/konu/3913121-c-terrain-bozuklugu-fix/
 https://forum.turkmmo.com/konu/3912251-maintenance-info-system/
