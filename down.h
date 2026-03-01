130660cccd6f5934c725267deb37d92003ffaf17
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-03-01 17:35:15 +0000

    Update down.h

--- down.h
+++ down.h
@@ -7,3 +7,5 @@ https://metin2.dev/topic/34006-remove-16bit-color-support-unused/
 https://metin2.dev/topic/33967-fix-questbuttons-out-of-questwindow-when-multiple-images-are-present/
 https://metin2.dev/topic/31778-remaining-book-count-in-skill-tooltip/
 https://metin2.dev/topic/33198-minimap-smooth-zoom/
+https://metin2.dev/topic/25874-offline-shop-premium-private-shop/
+
