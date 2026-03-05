231585739db6815c9d2f7426688686ea12ae9cd5
.mazeee1992 <162896786+maze92@users.noreply.github.com>
2026-03-05 03:34:32 +0000

    Update down.h

--- down.h
+++ down.h
@@ -1,12 +1,1 @@
-https://metin2.dev/topic/34347-query_boot-ub-fix/
-https://metin2.dev/topic/34299-c-gf-__loadatlasmarkinfo-whitemark-pixel-shift-fix/
-https://metin2.dev/topic/34261-c-metin2-client-window-centering-bug-win1011-%E2%80%93-real-cause-and-permanent-solution/
-https://metin2.dev/topic/34280-map-preview-scroll/
-https://metin2.dev/topic/34263-system-monitoring-cpu-gpu-ram-fps/
-https://metin2.dev/topic/34006-remove-16bit-color-support-unused/
-https://metin2.dev/topic/33967-fix-questbuttons-out-of-questwindow-when-multiple-images-are-present/
-https://metin2.dev/topic/31778-remaining-book-count-in-skill-tooltip/
-https://metin2.dev/topic/33198-minimap-smooth-zoom/
-https://metin2.dev/topic/25874-offline-shop-premium-private-shop/
-https://metin2.dev/topic/6314-ingame-wiki-system/
-
+https://forum.turkmmo.com/konu/3930936-c-fix-mini-harita-gorev-iconu/
